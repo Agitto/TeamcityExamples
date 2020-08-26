af74d894d664f87c3c8dcbc419c04aebf4749736
Vyacheslav Zaikin <agitto@yandex.ru>
2020-08-26 18:57:35 +0300

    add branch

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,7 +30,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 */
 version = "2019.1"
 
-class Repository constructor(val name: String, val url: String)
+class Repository constructor(val name: String, val url: String, val branch: String)
 
 class Build(val repo: Repository, val vcsRoot: GitVcsRoot) : BuildType({
     id(repo.name.toExtId())
@@ -50,8 +50,8 @@ class Build(val repo: Repository, val vcsRoot: GitVcsRoot) : BuildType({
 })
 
 val repositories = listOf(
-        Repository("xamarin_forms_demo_app","https://github.com/DevExpress-Examples/xamarin-forms-demo-app.git"),
-        Repository("xamarin_forms_stocks_app","https://github.com/DevExpress-Examples/xamarin-forms-stocks-app.git")
+        Repository("xamarin_forms_demo_app","https://github.com/DevExpress-Examples/xamarin-forms-demo-app.git", "20.1.3+"),
+        Repository("xamarin_forms_stocks_app","https://github.com/DevExpress-Examples/xamarin-forms-stocks-app.git", "19.2.1+")
 )
 
 project {
@@ -60,6 +60,7 @@ project {
             id(repo.name.toExtId())
             name = repo.name
             url = repo.url
+            branch = repo.branch
         }
         vcsRoot(vcs)
         buildType(Build(repo, vcs))
