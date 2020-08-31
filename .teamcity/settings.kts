3dbcd56d5a738ad82533b5a64fa9c2c2bdb6e4bd
Vyacheslav Zaikin <agitto@yandex.ru>
2020-08-31 07:29:45 +0300

    add sln

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -31,7 +31,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 */
 version = "2019.1"
 
-class Repository constructor(val name: String, val url: String, val branch: String)
+class Repository constructor(val name: String, val url: String, val sln: String, val branch: String)
 
 class Build(val repo: Repository, val vcsRoot: GitVcsRoot) : BuildType({
     id(repo.name.toExtId())
@@ -47,6 +47,7 @@ class Build(val repo: Repository, val vcsRoot: GitVcsRoot) : BuildType({
             noCache = true
             sources = "./packages"
             toolPath = "/Library/Frameworks/Mono.framework/Versions/Current/Commands/nuget"
+            projects = repo.sln
         }
 
         dotnetBuild {
@@ -71,8 +72,8 @@ class Build(val repo: Repository, val vcsRoot: GitVcsRoot) : BuildType({
 })
 
 val repositories = listOf(
-        Repository("xamarin_forms_demo_app","https://github.com/DevExpress-Examples/xamarin-forms-demo-app.git", "20.1.3+"),
-        Repository("xamarin_forms_stocks_app","https://github.com/DevExpress-Examples/xamarin-forms-stocks-app.git", "19.2.1+")
+        Repository("xamarin_forms_demo_app","https://github.com/DevExpress-Examples/xamarin-forms-demo-app.git", "DemoCenter.sln", "20.1.3+"),
+        Repository("xamarin_forms_stocks_app","https://github.com/DevExpress-Examples/xamarin-forms-stocks-app.git", "Stocks.sln","19.2.1+")
 )
 
 project {
